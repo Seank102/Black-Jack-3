bc5909f4d6248f904d573f3743941bb577b5fb42
Seank102 <Sean.robertson102@gmail.com>
2024-12-01 21:31:07 +0000

    Added a test deck function for debugging. Had to fix some code to make it work but it works now.

--- Deck.cpp
+++ Deck.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // Initializes the deck with all 52 cards
 Deck::Deck() {
-    static_assert(CLUBS < SPADES, "Suit enum values are not ordered properly."); // Safegaurd against accidental Suit enum changes
+    static_assert(CLUBS < DIAMONDS, "Suit enum values are not ordered properly."); // Safegaurd against accidental Suit enum changes
     static_assert(TWO < ACE, "Rank enum values are not ordered properly."); // Safegaurd against accidental Rank enum changes
 
     for (int s = CLUBS; s <= DIAMONDS; ++s) {  // Loop through all suits
@@ -19,7 +19,7 @@ Deck::Deck() {
 // Resets the deck to its original state
 void Deck::resetDeck() {
     cards.clear();  // Clear the current deck
-    for (int s = CLUBS; s <= SPADES; ++s) {  // Loop through all suits
+    for (int s = CLUBS; s <= DIAMONDS; ++s) {  // Loop through all suits
         for (int r = TWO; r <= ACE; ++r) {  // Loop through all ranks
             cards.push_back(Card(static_cast<Rank>(r), static_cast<Suit>(s)));
         }
@@ -49,8 +49,8 @@ void Deck::displayDeck() const {
     for (const auto& card : cards) {
         card.displayCard();
     }
+}
 
 int Deck::getDeckSize() const {
         return cards.size();
     }
-}
